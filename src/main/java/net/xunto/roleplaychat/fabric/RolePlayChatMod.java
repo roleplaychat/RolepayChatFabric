3c29a81c1f13f5449809181f8f61c7fb7829c7cc
xunto <xunto.orlov@gmail.com>
2023-02-05 17:35:14 +0300

    Revert to using server message, instead of trying to utilize signed messages.

--- src/main/java/net/xunto/roleplaychat/fabric/RolePlayChatMod.java
+++ src/main/java/net/xunto/roleplaychat/fabric/RolePlayChatMod.java
@@ -1,14 +1,6 @@
 package net.xunto.roleplaychat.fabric;
 
-import static net.minecraft.util.registry.BuiltinRegistries.MESSAGE_TYPE;
-
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.network.message.MessageType;
-import net.minecraft.text.Decoration;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.BuiltinRegistries;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.util.registry.RegistryKey;
 import net.xunto.roleplaychat.RoleplayChatCore;
 import net.xunto.roleplaychat.fabric.adapters.FabricLogger;
 import org.slf4j.Logger;
@@ -19,25 +11,9 @@ public class RolePlayChatMod implements ModInitializer {
   public static final Logger LOGGER = LoggerFactory.getLogger("roleplaychat");
   public static final FabricLogger FABRIC_LOGGER = new FabricLogger(LOGGER);
 
-  public static final RegistryKey<MessageType> ROLEPLAYCHAT = RegistryKey.of(
-      Registry.MESSAGE_TYPE_KEY,
-      new Identifier("roleplaychat")
-  );
-
-  public void registerMessageType() {
-    BuiltinRegistries.add(MESSAGE_TYPE,
-        ROLEPLAYCHAT, new MessageType(
-            Decoration.ofChat("%s%s"),
-            Decoration.ofChat("%s%s")
-        )
-    );
-  }
-
   @Override
   public void onInitialize() {
     RoleplayChatCore.instance.warmUpRenderer();
     RoleplayChatCore.instance.setLogger(FABRIC_LOGGER);
-
-    this.registerMessageType();
   }
 }
--- src/main/java/net/xunto/roleplaychat/fabric/adapters/FabricSpeaker.java
+++ src/main/java/net/xunto/roleplaychat/fabric/adapters/FabricSpeaker.java
@@ -1,16 +1,7 @@
 package net.xunto.roleplaychat.fabric.adapters;
 
 import java.util.Objects;
-import net.minecraft.network.message.MessageType;
-import net.minecraft.network.message.MessageType.Parameters;
-import net.minecraft.network.message.SentMessage;
-import net.minecraft.network.message.SignedMessage;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.xunto.roleplaychat.fabric.FabricComponents;
-import net.xunto.roleplaychat.fabric.RolePlayChatMod;
-import net.xunto.roleplaychat.fabric.framework.FabricRequest;
-import net.xunto.roleplaychat.framework.api.Request;
-import net.xunto.roleplaychat.framework.text.Text;
 
 public class FabricSpeaker extends FabricEntitySpeaker {
 
@@ -19,32 +10,6 @@ public class FabricSpeaker extends FabricEntitySpeaker {
   public FabricSpeaker(ServerPlayerEntity player) {
     super(player);
     this.player = player;
-  }
-
-  @Override
-  public void sendMessage(Request request, Text text) {
-    if (!(request instanceof FabricRequest fabricRequest)) {
-      super.sendMessage(request, text);
-    } else {
-      // Make signed content
-      SignedMessage signedMessage = fabricRequest.getMessage()
-          .withUnsignedContent(
-              FabricComponents.toTextComponent(text)
-          );
-
-      // Build parameters for new message
-      Parameters params = MessageType.params(
-          RolePlayChatMod.ROLEPLAYCHAT,
-          player.world.getRegistryManager(),
-          net.minecraft.text.Text.literal("")
-      );
-
-      player.sendChatMessage(
-          SentMessage.of(signedMessage),
-          false,
-          params
-      );
-    }
   }
 
   @Override
