63e3cef2567b37b911c397c70ccd7a5b5d2f1249
xunto <xunto.orlov@gmail.com>
2023-02-05 17:07:07 +0300

    Add logger.

--- src/main/java/net/xunto/roleplaychat/fabric/RolePlayChatMod.java
+++ src/main/java/net/xunto/roleplaychat/fabric/RolePlayChatMod.java
@@ -10,12 +10,14 @@ import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 import net.xunto.roleplaychat.RoleplayChatCore;
+import net.xunto.roleplaychat.fabric.adapters.FabricLogger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RolePlayChatMod implements ModInitializer {
 
   public static final Logger LOGGER = LoggerFactory.getLogger("roleplaychat");
+  public static final FabricLogger FABRIC_LOGGER = new FabricLogger(LOGGER);
 
   public static final RegistryKey<MessageType> ROLEPLAYCHAT = RegistryKey.of(
       Registry.MESSAGE_TYPE_KEY,
@@ -34,6 +36,7 @@ public class RolePlayChatMod implements ModInitializer {
   @Override
   public void onInitialize() {
     RoleplayChatCore.instance.warmUpRenderer();
+    RoleplayChatCore.instance.setLogger(FABRIC_LOGGER);
 
     this.registerMessageType();
   }
---
+++ src/main/java/net/xunto/roleplaychat/fabric/adapters/FabricLogger.java
@@ -0,0 +1,18 @@
+package net.xunto.roleplaychat.fabric.adapters;
+
+import net.xunto.roleplaychat.api.ILogger;
+import net.xunto.roleplaychat.framework.text.Text;
+import org.slf4j.Logger;
+
+public class FabricLogger implements ILogger {
+  private final Logger logger;
+
+  public FabricLogger(Logger logger) {
+    this.logger = logger;
+  }
+
+  @Override
+  public void log(Text text) {
+    this.logger.info(text.getUnformattedText());
+  }
+}
